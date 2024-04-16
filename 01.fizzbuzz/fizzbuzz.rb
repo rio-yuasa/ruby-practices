2e21e331097964f17bfd273d84f392d9463dd913
rio-yuasa <rio.yuasa@smarthr.co.jp>
2024-04-16 18:33:34 +0900

    fizzbuzzの課題作成

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+range = 1..20
+range.each do |num|
+    if num % 15 == 0
+        puts 'FizzBuzz'
+    elsif num % 5 == 0
+        puts 'Buzz'
+    elsif num % 3 == 0
+        puts 'Fizz'
+    else
+        puts num
+    end
+end
