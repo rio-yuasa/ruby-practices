0f5f557eee35721631bb2d17c91db05984af5e2c
rio-yuasa <rio.yuasa@smarthr.co.jp>
2024-04-24 20:20:26 +0900

    不要な変数を削除、変数名を変更、インデントを修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,13 @@
 #!/usr/bin/env ruby
 
-range = 1..20
-range.each do |num|
-    if num % 15 == 0
-        puts 'FizzBuzz'
-    elsif num % 5 == 0
-        puts 'Buzz'
-    elsif num % 3 == 0
-        puts 'Fizz'
-    else
-        puts num
-    end
+(1..20).each do |n|
+  if n % 15 == 0
+    puts "FizzBuzz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  else
+    puts n
+  end
 end
